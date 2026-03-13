5ab8f63c485acb5531fb2f7e29f264dc63d1ceed
jonathan <joni7840850@gmail.com>
2026-03-13 08:16:51 -0600

    Add addNode and deleteNode methods

--- src/main/java/com/kvstore/client/ClusterClient.java
+++ src/main/java/com/kvstore/client/ClusterClient.java
@@ -5,14 +5,16 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import com.kvstore.common.Node;
 import com.kvstore.common.exceptions.EmptyRingException;
+import com.kvstore.common.exceptions.NodeAlreadyInRingException;
+import com.kvstore.common.exceptions.NodeNotInRingException;
 import com.kvstore.consistenHashing.HashRingInterface;
 
 public class ClusterClient {
 
-  private HashRingInterface hashRing;
+  private final HashRingInterface hashRing;
   private ConcurrentHashMap<Node, KVClient> clientPool;
 
-  public ClusterClient(HashRingInterface hashRing) {
+  public ClusterClient(final HashRingInterface hashRing) {
     this.hashRing = hashRing;
     populateClientPool();
   }
@@ -22,38 +24,38 @@ public class ClusterClient {
     hashRing.getCopyOfNodesInRing()
         .stream()
         .forEach(n -> {
-          KVClient client = new KVClient(n.gethost(), n.getport());
+          final KVClient client = new KVClient(n.gethost(), n.getport());
         });
   }
 
-  public Optional<byte[]> getValue(String key) {
+  public Optional<byte[]> getValue(final String key) {
     try {
-      Node node = hashRing.determineNodeForKey(key);
-      KVClient client = clientPool.get(node);
+      final Node node = hashRing.determineNodeForKey(key);
+      final KVClient client = clientPool.get(node);
       return client.get(key);
-    } catch (EmptyRingException ex) {
+    } catch (final EmptyRingException ex) {
       return Optional.empty();
     }
   }
 
-  // TODO: should this throw? Mmm the ring is only used in this class so I think
-  // throwing
-  // an exception
-  // is fine but should be other like NoNodesException?
-  public void putValue(String key, byte[] value) throws EmptyRingException {
-    Node node = hashRing.determineNodeForKey(key);
-    KVClient client = clientPool.get(node);
+  public void putValue(final String key, final byte[] value) throws EmptyRingException {
+    final Node node = hashRing.determineNodeForKey(key);
+    final KVClient client = clientPool.get(node);
     client.put(key, value);
   }
 
-  // TODO: should this throw? Mmm the ring is only used in this class so I think
-  // throwing
-  // an exception
-  // is fine but should be other like NoNodesException?
-  public void deleteValue(String key) throws EmptyRingException {
-    Node node = hashRing.determineNodeForKey(key);
-    KVClient client = clientPool.get(node);
+  public void deleteValue(final String key) throws EmptyRingException {
+    final Node node = hashRing.determineNodeForKey(key);
+    final KVClient client = clientPool.get(node);
     client.delete(key);
+  }
+
+  public void addNode(final Node node) throws NodeAlreadyInRingException {
+    hashRing.addNode(node);
+  }
+
+  public void removeNode(final Node node) throws NodeNotInRingException {
+    hashRing.removeNode(node);
   }
 
 }
