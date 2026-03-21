7dafee851edd26b5114580be51edf7a46381a926
jonathan <joni7840850@gmail.com>
2026-03-21 16:01:52 -0600

    Update existing test to new Set<Node> method signature

--- src/test/java/com/kvstore/HashRingTest.java
+++ src/test/java/com/kvstore/HashRingTest.java
@@ -10,9 +10,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.kvstore.common.Node;
-import com.kvstore.common.exceptions.EmptyRingException;
 import com.kvstore.common.exceptions.NodeAlreadyInRingException;
 import com.kvstore.common.exceptions.NodeNotInRingException;
+import com.kvstore.common.exceptions.NotEnoughNodesException;
 import com.kvstore.consistenHashing.HashRing;
 
 public class HashRingTest {
@@ -29,29 +29,30 @@ public class HashRingTest {
 
   @Test
   public void addNodeThatAlreadyExistsExceptionTest() throws NodeAlreadyInRingException {
-    Node node = new Node(host, port);
+    final Node node = new Node(host, port);
     hashRing.addNode(node);
     assertThrows(NodeAlreadyInRingException.class, () -> hashRing.addNode(node));
   }
 
   @Test
   public void getNodeWithNoNodesExceptionTest() {
-    String key = "key";
-    assertThrows(EmptyRingException.class, () -> hashRing.determineNodeForKey(key));
+    final String key = "key";
+    final int PARTITION_FACTOR = 1;
+    assertThrows(NotEnoughNodesException.class, () -> hashRing.determineNodesForKey(key, PARTITION_FACTOR));
   }
 
   @Test
   public void removeNodeThatIsNotInTheRingExceptionTest() {
-    Node node = new Node(host, port);
+    final Node node = new Node(host, port);
     assertThrows(NodeNotInRingException.class, () -> hashRing.removeNode(node));
   }
 
   @Test
   public void addMultipleNodeTest() throws NodeAlreadyInRingException {
     final int EXPECTED_NUMBER_NODES = 2;
-    Node node1 = new Node(host + "1", port);
+    final Node node1 = new Node(host + "1", port);
     hashRing.addNode(node1);
-    Node node2 = new Node(host + "2", port);
+    final Node node2 = new Node(host + "2", port);
     hashRing.addNode(node2);
     assertEquals(hashRing.getCopyOfNodesInRing().size(), EXPECTED_NUMBER_NODES);
   }
@@ -59,52 +60,56 @@ public class HashRingTest {
   @Test
   public void removeNodeTest() throws NodeAlreadyInRingException, NodeNotInRingException {
     final int EXPECTED_NUMBER_NODES = 0;
-    Node node = new Node(host, port);
+    final Node node = new Node(host, port);
     hashRing.addNode(node);
     hashRing.removeNode(node);
     assertEquals(hashRing.getCopyOfNodesInRing().size(), EXPECTED_NUMBER_NODES);
   }
 
   @Test
-  public void getNodeTest() throws NodeAlreadyInRingException, EmptyRingException {
-    Node node = new Node(host, port);
+  public void getNodeTest() throws NodeAlreadyInRingException, NotEnoughNodesException {
+    final Node node = new Node(host, port);
+    final int PARTITION_FACTOR = 1;
     hashRing.addNode(node);
-    String key = "key";
-    Node providedNode = hashRing.determineNodeForKey(key);
+    final String key = "key";
+    final Node providedNode = hashRing.determineNodesForKey(key, PARTITION_FACTOR)
+        .stream().findFirst().get();
     assertEquals(node, providedNode);
   }
 
   @Test
-  public void checkDistributionOfKeys() throws NodeAlreadyInRingException, EmptyRingException {
+  public void checkDistributionOfKeys() throws NodeAlreadyInRingException, NotEnoughNodesException {
 
-    int numberOfVirtualNodes = 150;
+    final int numberOfVirtualNodes = 150;
     this.hashRing = new HashRing(numberOfVirtualNodes);
-    int numberOfNodes = 4;
-    int numberOfKeys = 10_000;
+    final int numberOfNodes = 4;
+    final int numberOfKeys = 10_000;
+    final int PARTITION_FACTOR = 1;
 
-    int expectedKeysPerNode = numberOfKeys / numberOfNodes;
-    int lowerExpectedKeysPerNode = (int) (expectedKeysPerNode * 0.5);
-    int upperExpectedKeysPerNode = (int) (expectedKeysPerNode * 1.5);
+    final int expectedKeysPerNode = numberOfKeys / numberOfNodes;
+    final int lowerExpectedKeysPerNode = (int) (expectedKeysPerNode * 0.5);
+    final int upperExpectedKeysPerNode = (int) (expectedKeysPerNode * 1.5);
 
-    HashMap<Node, Integer> keysPerNode = new HashMap<>();
-    Node[] nodes = new Node[numberOfNodes];
+    final HashMap<Node, Integer> keysPerNode = new HashMap<>();
+    final Node[] nodes = new Node[numberOfNodes];
 
     for (int i = 0; i < numberOfNodes; i++) {
-      String host = "host" + i;
-      int port = 8088 + i;
+      final String host = "host" + i;
+      final int port = 8088 + i;
       nodes[i] = new Node(host, port);
       keysPerNode.put(nodes[i], 0);
       hashRing.addNode(nodes[i]);
     }
 
     for (int i = 0; i < numberOfKeys; i++) {
-      String key = "key" + i;
-      Node node = hashRing.determineNodeForKey(key);
+      final String key = "key" + i;
+      final Node node = hashRing.determineNodesForKey(key, PARTITION_FACTOR)
+          .stream().findFirst().get();
       keysPerNode.put(node, keysPerNode.get(node) + 1);
     }
 
-    for (Node node : keysPerNode.keySet()) {
-      int keysInThisNode = keysPerNode.get(node);
+    for (final Node node : keysPerNode.keySet()) {
+      final int keysInThisNode = keysPerNode.get(node);
       assertTrue(keysInThisNode >= lowerExpectedKeysPerNode
           && keysInThisNode <= upperExpectedKeysPerNode);
     }
