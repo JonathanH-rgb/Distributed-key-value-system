faa9236d1729529f9bbcbfe376696d6538b50942
jonathan <joni7840850@gmail.com>
2026-03-21 15:56:43 -0600

    Create wrapper to have byte[] value and version timestamp encoded as long of a value

---
+++ src/main/java/com/kvstore/common/VersionedValue.java
@@ -0,0 +1,29 @@
+package com.kvstore.common;
+
+public class VersionedValue {
+
+  private byte[] bytes;
+  private long version;
+
+  public VersionedValue(byte[] bytes, long version) {
+    this.bytes = bytes;
+    this.version = version;
+  }
+
+  public void setVersion(long version) {
+    this.version = version;
+  }
+
+  public long getVersion() {
+    return version;
+  }
+
+  public byte[] getBytes() {
+    return bytes;
+  }
+
+  public void setBytes(byte[] bytes) {
+    this.bytes = bytes;
+  }
+
+}
