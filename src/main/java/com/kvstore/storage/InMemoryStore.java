db013b26bac4d094edbdb7f91605fd3a43664ad2
jonathan <joni7840850@gmail.com>
2026-03-21 15:58:36 -0600

    Use versioned value instead of byte[]

--- src/main/java/com/kvstore/storage/InMemoryStore.java
+++ src/main/java/com/kvstore/storage/InMemoryStore.java
@@ -4,24 +4,27 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 
+import com.kvstore.common.VersionedValue;
+
 /**
  * In-memory implementation of StorageEngine.
- * Uses a ConcurrentHashMap to safely handle concurrent reads and writes from multiple threads.
+ * Uses a ConcurrentHashMap to safely handle concurrent reads and writes from
+ * multiple threads.
  */
 public class InMemoryStore implements StorageEngine {
 
-  private Map<String, byte[]> memoryStorage;
+  private Map<String, VersionedValue> memoryStorage;
 
   public InMemoryStore() {
     this.memoryStorage = new ConcurrentHashMap<>();
   }
 
-  public Optional<byte[]> get(String key) {
+  public Optional<VersionedValue> get(String key) {
     return Optional.ofNullable(memoryStorage.get(key));
   }
 
-  public void put(String key, byte[] value) {
-    memoryStorage.put(key, value);
+  public void put(String key, byte[] value, long version) {
+    memoryStorage.put(key, new VersionedValue(value, version));
   }
 
   public void delete(String key) {
