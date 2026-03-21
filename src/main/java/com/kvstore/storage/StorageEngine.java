6151fbeb41723efbf83c28a6fb4f1b3e873cd856
jonathan <joni7840850@gmail.com>
2026-03-21 15:58:49 -0600

    Use versioned value instead of byte[]

--- src/main/java/com/kvstore/storage/StorageEngine.java
+++ src/main/java/com/kvstore/storage/StorageEngine.java
@@ -2,15 +2,18 @@ package com.kvstore.storage;
 
 import java.util.Optional;
 
+import com.kvstore.common.VersionedValue;
+
 /**
  * Contract for the key-value storage layer.
- * Implementations are responsible for storing, retrieving, and deleting values by key.
+ * Implementations are responsible for storing, retrieving, and deleting values
+ * by key.
  */
 public interface StorageEngine {
 
-  Optional<byte[]> get(String key);
+  Optional<VersionedValue> get(String key);
 
-  void put(String key, byte[] value);
+  void put(String key, byte[] value, long version);
 
   void delete(String key);
 }
