e8266fae618f338d6cde329a5f6ed38b60a3a276
jonathan <joni7840850@gmail.com>
2026-04-01 20:31:55 -0600

    Add hardcode node

--- src/main/java/com/kvstore/server/ServerMain.java
+++ src/main/java/com/kvstore/server/ServerMain.java
@@ -1,13 +1,16 @@
 package com.kvstore.server;
 
+import com.kvstore.common.Node;
+import com.kvstore.common.exceptions.EmptyHardcodedNodesListException;
+
 public class ServerMain {
 
   public ServerMain() {
-
   }
 
-  public static void main(String[] args) {
-    KVServer kvServer = new KVServer();
+  public static void main(String[] args) throws EmptyHardcodedNodesListException {
+    Node[] hardcodedNodesInfo = new Node[] { new Node("host", 8080) };
+    KVServer kvServer = new KVServer(hardcodedNodesInfo);
     kvServer.start(8088);
   }
 
