b160c6a2bd029077dadcfbd90b030d96426ed05b
jonathan <joni7840850@gmail.com>
2026-05-03 22:18:17 -0600

    Add recover method to return map status from snapshot and WAL

--- src/main/java/com/kvstore/storage/DurableStorageEngine.java
+++ src/main/java/com/kvstore/storage/DurableStorageEngine.java
@@ -3,10 +3,15 @@ package com.kvstore.storage;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import com.kvstore.common.VersionedValue;
+import com.kvstore.common.exceptions.SnapshotCouldNotReadException;
+import com.kvstore.common.exceptions.SnapshotCouldNotWriteException;
 import com.kvstore.common.exceptions.StorageException;
-import com.kvstore.common.exceptions.WALCouldNotOpenLogFileException;
+import com.kvstore.common.exceptions.WALCouldNotReadLogFileException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,6 +25,10 @@ public class DurableStorageEngine implements StorageEngine {
 
   private static final Logger logger = LoggerFactory.getLogger(DurableStorageEngine.class);
 
+  private static final int THREADS_RUNNING_SNAPSHOT = 1;
+  private static final long SNAPSHOT_LOOP_DELAY_SECS = 0;
+  private static final long SNAPSHOT_FREQ_SECS = 3600;
+
   private Map<String, VersionedValue> memoryStorage = new ConcurrentHashMap<>();
   private final WriteAheadLogInterface writeAheadLog;
   private final SnapShotManagerInterface snapShotManager;
@@ -27,6 +36,33 @@ public class DurableStorageEngine implements StorageEngine {
   public DurableStorageEngine(WriteAheadLogInterface writeAheadLog, SnapShotManagerInterface snapShotManager) {
     this.writeAheadLog = writeAheadLog;
     this.snapShotManager = snapShotManager;
+  }
+
+  private void recover() throws StorageException {
+    try {
+      long latestSnapshotTime = snapShotManager.latestSnapshotTime();
+      memoryStorage = snapShotManager.recover();
+      Map<String, VersionedValue> walRemaining = writeAheadLog.recover(latestSnapshotTime);
+      for (String key : walRemaining.keySet()) {
+        memoryStorage.put(key, walRemaining.get(key));
+      }
+    } catch (SnapshotCouldNotReadException | WALCouldNotReadLogFileException ex) {
+      throw new StorageException("Failed to recover storage engine", ex);
+    }
+  }
+
+  public void start() throws StorageException {
+    recover();
+    ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(THREADS_RUNNING_SNAPSHOT);
+    scheduler.scheduleAtFixedRate(() -> {
+      try {
+        snapShotManager.snapshot(memoryStorage);
+      } catch (SnapshotCouldNotWriteException ex) {
+        logger.error("Snapshot failed ", ex);
+      }
+    }, SNAPSHOT_LOOP_DELAY_SECS,
+        SNAPSHOT_FREQ_SECS,
+        TimeUnit.SECONDS);
   }
 
   public Optional<VersionedValue> get(String key) {
